315f9fca122682b369e634451a2dd661effa6c96
phyopine <phyo.pine@gmail.com>
2015-10-06 20:38:22 -0700

    Add soon to expire cards

--- server.js
+++ server.js
@@ -3,55 +3,75 @@ var express = require('express'),
 	stripe = require("stripe")(
 		"sk_test_PemncDJakbsXxUgyBe4uodxN"
 	);
+	
+var today = new Date(),
+	past_week = new Date(),
+	last_month = new Date();
 
+	past_week.setDate(past_week.getDate()-7);
+	last_month.setMonth(last_month.getMonth()-1);
+		
+var	unix_past_week = getUnixDate(past_week),
+	unix_last_month = getUnixDate(last_month),
+	successful_charge = 0,
+	successful_amount = 0;	
+		
 stripe.charges.list(
+  { "created[gt]": unix_last_month, limit: 1000},
   function(err, charges) {
-  	/*
-	app.get('/', function (req, res) {
-	  res.send(charges.data);
-	});
-	*/
-	var past_week = new Date();
-		past_week.setDate(past_week.getDate()-7);
-	var	unix_past_week = Date.parse(past_week)/1000;
-	var successful_charge = 0,
-		successful_amount = 0;	
 	for(var i = 0; i < charges.data.length; i++) {
-		var newCharge = new Charge();
-		newCharge.id = charges.data[i].id;
-		newCharge.amount = charges.data[i].amount;
-		newCharge.paid = charges.data[i].paid;
-		newCharge.status = charges.data[i].status;
-		newCharge.created = charges.data[i].created;
-		if(newCharge.status == 'succeeded' && newCharge.paid == true){
-			if(newCharge.created >= unix_past_week) {
+		if(charges.data[i].status == 'succeeded' && charges.data[i].paid == true){
+			if(charges.data[i].created >= unix_past_week) {
 				successful_charge += 1;
-				successful_amount += newCharge.amount;
+				successful_amount += charges.data[i].amount;
 			}
 		}
+		
+		if(charges.data[i].failure_code) 
+			console.log(charges.data[i].failure_code);
+		if(charges.data[i].status == 'failed' && charges.data[i].card_error == 'card_declined' ) {
+			if(charges.data[i].created >= unix_last_month) {
+				console.log(charges.data[i]);
+			}
+		}
+		//charges have customers
+		if(charges.data[i].customer) {
+			stripe.customers.listCards(charges.data[i].customer, function(err, cards) {
+				for(var c = 0; c < cards.data.length; c++) {
+					//same year, near to expire but can be better here.
+					if(cards.data[c].exp_year == today.getFullYear()) {
+						console.log(cards.data[c]);
+					}
+				}
+			});
+		}
 	  }
-	 var rJSON = getSevenDayRevenue(successful_amount, successful_charge, unix_past_week);
+	
+	var sevenDayRevenue = getSevenDayRevenue(successful_amount, successful_charge, past_week);
+	
+		
+	
+	allResults = new returnObj(today, sevenDayRevenue) ;
 	  
-	  show_on_server(rJSON);
+	rJSON = JSON.stringify(allResults);
+	//show_on_server(charges);
+	//show_on_server(allResults);
 	  
 });	
 
-function getSevenDayRevenue(successful_amount, successful_charge, unix_past_week) {
-	var r = new sevenDayRevenue();
-	  r.total_amount = successful_amount;
-	  r.total_charge = successful_charge;
-	  r.start_datetime = unix_past_week;
-	  rJSON = JSON.	stringify(r);
-	  
-	  return rJSON;
-}
 
- function Charge(id, amount, paid, status, created) {
-	this.id = id;
-	this.amount = amount;
-	this.paid = paid;
-	this.status = status;
-	this.created = created;
+function returnObj (today, sevenDayRevenue) {
+	this.today = today;
+	this.sevenDayRevenue = sevenDayRevenue;
+
+}
+ 
+ function getSevenDayRevenue(total_amount, total_charge, start_datetime){
+	var r = new sevenDayRevenue();
+		r.total_amount = total_amount;
+		r.total_charge = total_charge;
+		r.start_datetime = start_datetime;
+	return r;
  }
  
  function sevenDayRevenue(total_amount, total_charge, start_datetime) {
@@ -61,6 +81,11 @@ function getSevenDayRevenue(successful_amount, successful_charge, unix_past_week
 	this.start_datetime = start_datetime;
  }
 
+function getUnixDate(date_obj){
+	return Date.parse(date_obj)/1000;
+} 
+ 
+ 
 function show_on_server(rJSON) {
 	  app.get('/', function (req, res) {
 		  res.send(rJSON);
